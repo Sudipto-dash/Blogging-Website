ec9c31549d81f41444537a207aff9526b19b90a9
Sudipto-dash <sudiptodsh@gmail.com>
2022-06-06 23:09:52 +0600

    Updated
    Updated

--- blogBD/blogBDapp/views.py
+++ blogBD/blogBDapp/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+import imp
+from pydoc import pager
+from django.shortcuts import redirect, render
 from matplotlib.style import context
+from django.core.paginator import PageNotAnInteger,EmptyPage,Paginator
 
 from .models import (
     Blog,
@@ -18,4 +21,20 @@ def home(request):
     }
     return render(request,'home.html',context)
 def blogs(request):
-    return render(request,'blogs.html')
+    queryset = Blog.objects.order_by('-created_date')
+    tags = Tag.objects.order_by('-created_date')
+    page = request.GET.get('page', 1)
+    paginator = Paginator(queryset, 2) #no. of blogs showing
+    try:
+        blogs = paginator.page(page)
+    except EmptyPage:
+        blogs = paginator.page(1)
+    except PageNotAnInteger:
+        blogs = paginator.page(1)
+        return redirect("blogs")
+    context = {
+        "blogs": blogs,
+        "tags": tags,
+        "paginator":paginator
+    }
+    return render(request,'blogs.html',context)
