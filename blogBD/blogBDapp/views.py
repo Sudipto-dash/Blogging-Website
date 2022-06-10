df7e045b62067f5625de128984eec641f323bf29
Sudipto-dash <sudiptodsh@gmail.com>
2022-06-11 02:35:32 +0600

    Updated
    Updated

--- blogBD/blogBDapp/views.py
+++ blogBD/blogBDapp/views.py
@@ -1,6 +1,7 @@
 import imp
 from pydoc import pager
-from django.shortcuts import redirect, render
+from unicodedata import category
+from django.shortcuts import get_object_or_404, redirect, render
 from matplotlib.style import context
 from django.core.paginator import PageNotAnInteger,EmptyPage,Paginator
 
@@ -38,3 +39,47 @@ def blogs(request):
         "paginator":paginator
     }
     return render(request,'blogs.html',context)
+
+def category_blogs(request,slug):
+    category = get_object_or_404(Category,slug=slug)
+    tags = Tag.objects.order_by('-created_date')[:5] #Showing 5 tags
+    queryset = category.category_blogs.all()
+    all_blog =Blog.objects.order_by('-created_date')[:5]
+    page = request.GET.get('page', 1)
+    paginator = Paginator(queryset, 2) #no. of blogs showing
+
+    try:
+        blogs = paginator.page(page)
+    except EmptyPage:
+        blogs = paginator.page(1)
+    except PageNotAnInteger:
+        blogs = paginator.page(1)
+        return redirect("blogs")
+    context = {
+        "blogs": blogs,
+        "tags": tags,
+        "all_blog": all_blog
+    }
+    return render(request,'category_blogs.html',context)
+
+def tag_blogs(request,slug):
+    tag = get_object_or_404(Tag,slug=slug)
+    tags = Tag.objects.order_by('-created_date')[:5] #Showing 5 tags
+    queryset = tag.tag_blogs.all()
+    all_blog =Blog.objects.order_by('-created_date')[:5]
+    page = request.GET.get('page', 1)
+    paginator = Paginator(queryset, 2) #no. of blogs showing
+
+    try:
+        blogs = paginator.page(page)
+    except EmptyPage:
+        blogs = paginator.page(1)
+    except PageNotAnInteger:
+        blogs = paginator.page(1)
+        return redirect("blogs")
+    context = {
+        "blogs": blogs,
+        "tags": tags,
+        "all_blog": all_blog
+    }
+    return render(request,'tag_blogs.html',context)
